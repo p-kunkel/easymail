c3fdcba9913bb8253535ce6a0f8029ff0f521bed
Patryk Kunkel <pat.kun98@gmail.com>
2023-06-29 16:15:14 +0200

    attachments

--- attachment.go
+++ attachment.go
@@ -3,9 +3,11 @@ package gosimplemime
 import (
 	"bytes"
 	"encoding/base64"
+	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"net/textproto"
+	"path/filepath"
 )
 
 type Attachment struct {
@@ -14,13 +16,47 @@ type Attachment struct {
 	File        bytes.Buffer
 }
 
+func NewAttachment() *Attachment {
+	return &Attachment{
+		File: bytes.Buffer{},
+	}
+}
+
+func (a *Attachment) ReadFile(path string) error {
+	f, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	if _, err = a.Write(f); err != nil {
+		return err
+	}
+
+	if a.Filename == "" {
+		a.Filename = filepath.Base(path)
+	}
+
+	if a.ContentType == "" {
+		a.DetectContentType()
+	}
+	return nil
+}
+
+func (a *Attachment) Write(b []byte) (int, error) {
+	return a.File.Write(b)
+}
+
+func (a *Attachment) DetectContentType() {
+	a.ContentType = http.DetectContentType(a.File.Bytes())
+}
+
 func (a *Attachment) CreateAndWritePartTo(mpw *multipart.Writer) error {
 	cp, err := mpw.CreatePart(a.GetHeaders())
 	if err != nil {
 		return err
 	}
 
-	dst := []byte{}
+	dst := make([]byte, base64.StdEncoding.EncodedLen(a.File.Len()))
 	base64.StdEncoding.Encode(dst, a.File.Bytes())
 	_, err = cp.Write(dst)
 	return err
@@ -28,7 +64,7 @@ func (a *Attachment) CreateAndWritePartTo(mpw *multipart.Writer) error {
 
 func (a *Attachment) GetHeaders() textproto.MIMEHeader {
 	if a.ContentType == "" {
-		a.ContentType = http.DetectContentType(a.File.Bytes())
+		a.DetectContentType()
 	}
 
 	h := make(textproto.MIMEHeader)
